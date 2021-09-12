851a7f1ecea607ed8b85236124664d5746e79198
Pratik Gohil <pr.tikgohil@gmail.com>
2021-09-12 21:14:30 +0530

    form test

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ contact_form.addEventListener("submit", (e) => {
   })
     .then((res) => console.log("res", res))
     .catch((err) => console.error(err));
-  this.reset();
+  form.reset();
 });
 
 window.onload = function () {
